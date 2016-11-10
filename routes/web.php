61f1f6d3ebdd4bf13668755317b9e59be2f8dc3a
Aurélio Guedes <aureliomachados@gmail.com>
2016-11-10 14:09:39 -0200

    resolving cors

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 //used to disable CORS
-//header('Access-Control-Allow-Origin:  *');
-//header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-//header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 /* 
     Rotas do Cidades API
